43efa8ccacacbdb31ebedc8d94c885861a71a22b
Sanskar-Pawar <sanskar.pawar@coditas.com>
2022-09-19 11:22:58 +0530

    initial commit

---
+++ app/src/main/java/com/coditas/ism/dateshowdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coditas.ism.dateshowdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
